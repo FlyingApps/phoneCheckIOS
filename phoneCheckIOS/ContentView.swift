d8e0330d60031a587dc38f4aad8b7a04a118300b
42JINAM <jinam@student.42.fr>
2025-06-16 11:05:10 +0200

    Initial Commit

---
+++ phoneCheckIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  phoneCheckIOS
+//
+//  Created by jinam on 6/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ phoneCheckIOS/phoneCheckIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  phoneCheckIOSApp.swift
+//  phoneCheckIOS
+//
+//  Created by jinam on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct phoneCheckIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
